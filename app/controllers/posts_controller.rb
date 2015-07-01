40e36557d01b2d16e6b799f4e315eed33385d7c6
Sam Waller <samuellwaller@gmail.com>
2015-07-01 07:28:01 -0400

    updating new repository

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.create
+  end
+
+  def create
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:avatar, :caption)
+  end
+
 end
