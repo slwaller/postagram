47fa3e8e354ea475c634b51a8c7f878f45cf31bb
Sam Waller <samuellwaller@gmail.com>
2015-07-03 04:13:31 -0400

    DRYing up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find params[:id]
   end
 
   def new
@@ -12,11 +13,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find params[:id]
   end
 
   def update
-    @post = Post.find params[:id]
     @post.update_attributes post_params
     redirect_to post_path(@post)
   end
@@ -32,13 +31,17 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find params[:id]
     @post.delete
     redirect_to posts_path
   end
 
 
   private
+
+  def set_post
+    @post = Post.find params[:id]
+  end
+
 
   def post_params
     params.require(:post).permit(:avatar, :caption)
