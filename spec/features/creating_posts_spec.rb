43b285df38b3d52eccffec54f6be75f3db56b24c
Sam Waller <samuellwaller@gmail.com>
2015-08-28 00:27:47 -0400

    setting up user authentication

---
+++ spec/features/creating_posts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper.rb'
+
+feature 'Creating posts' do
+  scenario 'can create a job' do
+    visit '/'
+    click_link 'New Post'
+    attach_file('Avatar', "spec/files/images/coffee.jpg")
+    fill_in 'Caption', with: 'nomnomnom #coffeetime'
+    click_button 'Create Post'
+    expect(page).to have_content('#coffeetime')
+    expect(page).to have_css("img[src*='coffee.jpg']")
+  end
+end
