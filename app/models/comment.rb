b79094b230e9af611f97562d066ed46f870280d1
Sam Waller <samuellwaller@gmail.com>
2015-09-08 01:09:49 -0400

    time posted inserted and validated length of comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+  validates_length_of :content, :minimum => 3, :maximum => 250, :allow_blank => false
+
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :avatar, presence: true
   validates :user_id, presence: true
+  
 
   has_attached_file :avatar, styles: { :medium => "640x" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
