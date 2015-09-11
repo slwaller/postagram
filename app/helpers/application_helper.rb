c8bdf0895f2f3f9d9f667afbd3ccf44c1cbfc326
Sam Waller <samuellwaller@gmail.com>
2015-09-11 02:48:56 -0400

    DRYing up and giving preview to new post as well

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
   }[flash_type.to_sym] || flash_type.to_s
   end  
 
+
+  def form_image_select(post)
+    return image_tag post.avatar.url(:medium),
+      id: 'image-preview',
+      class: 'img-responsive' if post.avatar.exists?
+    image_tag 'bgposta.jpg', id: 'image-preview', class: 'img-responsive'
+  end
 end
