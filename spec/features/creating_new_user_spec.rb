d07fd5f291e114904d23056d4985cd807ba770f0
Sam Waller <samuellwaller@gmail.com>
2015-09-16 00:29:20 -0400

    rspec test done

--- spec/features/creating_new_user_spec.rb
+++ spec/features/creating_new_user_spec.rb
@@ -14,4 +14,13 @@ feature 'Creating a new user' do
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
+
+  scenario 'requires a user name to successfully create an account' do
+    fill_in 'Email', with: 'rspecuser@mailinator.com'
+    fill_in 'Password', with: 'password', match: :first
+    fill_in 'Confirm Password', with: 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content("can't be blank")
+  end
 end
