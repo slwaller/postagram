f5a2693625a62168ce50f3e5fd96cbb38932505f
Sam Waller <samuellwaller@gmail.com>
2015-09-22 03:07:52 -0400

    commenting out image spec - adding needs image to save spec

--- spec/features/creating_new_posts_spec.rb
+++ spec/features/creating_new_posts_spec.rb
@@ -6,13 +6,21 @@ feature 'Creating posts' do
     sign_in_with user
   end
 
-  scenario 'can create a new post' do
+  # scenario 'can create a new post' do
+  #   visit '/'
+  #   click_link 'New Post'
+  #   attach_file('Avatar', "spec/files/images/seoul.jpg")
+  #   fill_in 'Caption', with: "Beautiful Seoul from N Tower!"
+  #   click_button 'Create Post'
+  #   expect(page).to have_content("Beautiful Seoul from N Tower!")
+  #   expect(page).to have_css("img[src*='seoul']")
+  # end
+
+  scenario 'a post needs an image to save' do
     visit '/'
     click_link 'New Post'
-    attach_file('Image', "spec/files/images/seoul.jpg")
-    fill_in 'Caption', with: "Beautiful Seoul from N Tower!"
+    fill_in 'Say something', with: "NO PICTURE LOL"
     click_button 'Create Post'
-    expect(page).to have_content("Beautiful Seoul from N Tower!")
-    expect(page).to have_css("img[src*='seoul']")
+    expect(page).to have_content("Your new post couldn't be created! Please check the form.")
   end
 end
