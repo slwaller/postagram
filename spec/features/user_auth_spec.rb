42ec13f90debb03db059383ef7597fa8dde7d151
Sam Waller <samuellwaller@gmail.com>
2015-09-28 01:07:43 -0400

    logout spec

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -18,4 +18,15 @@ feature 'User authentication' do
     expect(page).to_not have_content('Register')
     expect(page).to have_content('Logout')
   end
+
+    scenario 'can log out once logged in' do
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: 'rspecuser@mailinator.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+    click_link 'Logout'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
 end
