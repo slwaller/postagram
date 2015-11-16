38561d9fe9af2525aad5cb1a4a7c752918af2d0b
Sam Waller <samuellwaller@gmail.com>
2015-11-16 06:41:39 -0500

    user profile specs

---
+++ spec/features/view_user_profile_spec.rb
@@ -0,0 +1,15 @@
+feature 'viewing user profiles' do
+  background do
+    # Create a user.
+    # Create a second user
+    # Create a post owned by the user.
+    # Create a post owned by the second user.
+    # Sign in with the first user. 
+    # Visit the dashboard.
+    # Click the first user's user name
+  end
+
+  scenario 'visiting a profile page shows the username in URL' do
+    # expect the page URL to be /users/username
+  end
+end
