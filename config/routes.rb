0278290cea6f1c0a2a5e310f36eed49e3a04d006
Sam Waller <samuellwaller@gmail.com>
2015-11-19 06:07:27 -0500

    setting up profile page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get ':user_name', to: 'profiles#show', as: :profile 
   # Example resource route with options:
   #   resources :products do
   #     member do
