bf2fdfac06fd3176c3be5c322aae8deb53062333
Sam Waller <samuellwaller@gmail.com>
2015-12-01 03:56:57 -0500

    adding user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :avatar, styles: { medium: '152x152#' }  
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/  
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 end
