7c6418c987a4086ef2968bcada5e8c4093aa321e
Sam Waller <samuellwaller@gmail.com>
2015-12-21 04:16:13 -0500

    profile update route

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,21 @@ class ProfilesController < ApplicationController
     @user = User.find_by(user_name: params[:user_name])
   end  
 
-  def update
+  def update  
+    @user = User.find_by(user_name: params[:user_name])
+    if @user.update(profile_params)
+      flash[:success] = 'Your profile has been updated.'
+      redirect_to profile_path(@user.user_name)
+    else
+      @user.errors.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
   end
+private
+
+def profile_params  
+  params.require(:user).permit(:avatar, :bio)
+end
+
 end
