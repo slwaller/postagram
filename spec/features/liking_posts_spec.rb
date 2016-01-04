bdb12fc760bd3472cbba57152e56d6493fd28760
Sam Waller <samuellwaller@gmail.com>
2016-01-04 14:36:33 -0500

    initializing spec tests for like feature

--- spec/features/liking_posts_spec.rb
+++ spec/features/liking_posts_spec.rb
@@ -9,3 +9,10 @@ scenario 'can like a post' do
   # Expect to see a "liked-post" class appear for the button (the button will turn solid red aka Instagram).
   # Expect to see my name within the "liked by" area of the post.
 end
+
+scenario 'can unlike a post' do  
+  # Repeat the above steps.
+  # Click the "like" button once more.
+  # Expect to see the "unlikes-post" class on the button.
+  # Expect to NOT see my name within the "liked by" area of the post
+end  
