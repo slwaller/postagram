1de766a6b0c3648071b507b8f91544af5cd77d51
Sam Waller <samuellwaller@gmail.com>
2016-01-04 14:36:13 -0500

    initializing spec tests for like feature

---
+++ spec/features/liking_posts_spec.rb
@@ -0,0 +1,11 @@
+background do  
+  # Create a user via factory_girl
+  # Create a post for the above user.
+  # Visit the root route
+end
+
+scenario 'can like a post' do  
+  # Click the "like" button for the above post.
+  # Expect to see a "liked-post" class appear for the button (the button will turn solid red aka Instagram).
+  # Expect to see my name within the "liked by" area of the post.
+end
