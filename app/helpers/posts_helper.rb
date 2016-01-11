184915b663351b1b9bb41b6be7d0281a3ad49001
Sam Waller <samuellwaller@gmail.com>
2016-01-11 13:43:00 -0500

    setting up partial and helper for likes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,30 @@
-module PostsHelper
-end
+module PostsHelper  
+  # Our new helper method
+  def likers_of(post)
+    # votes variable is set to the likes by users.
+    votes = post.votes_for.up.by_type(User)
+    # set user_names variable as an empty array
+    user_names = []
+    # unless there are no likes, continue below.
+    unless votes.blank?
+      # iterate through the voters of each vote (the users who liked the post)
+      votes.voters.each do |voter|
+        # add the user_name as a link to the array
+        user_names.push(link_to voter.user_name,
+                                profile_path(voter.user_name),
+                                class: 'user-name')
+      end
+      # present the array as a nice sentence using the as_sentence method and also make it usable within our html.  Then call the like_plural method with the votes variable we set earlier as the argument.
+      user_names.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  private
+
+  def like_plural(votes)
+    # If we more than one like for a post, use ' like this'
+    return ' like this' if votes.count > 1
+    # Otherwise, return ' likes this'
+    ' likes this'
+  end
+end  
