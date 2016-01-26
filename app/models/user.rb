7cb77cfc48c487f54587503108f38e92f9470e40
Sam Waller <samuellwaller@gmail.com>
2016-01-25 19:52:11 -0500

    setting users acts as votable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  acts_as_voter
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
